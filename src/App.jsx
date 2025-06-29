ceb24a856aee4bde9f829ae3afd979617ba352fe
Anurup Reddy <101058877+anurupreddy127@users.noreply.github.com>
2025-06-29 15:13:38 -0500

    Updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import Header from './components/Header'
 import Hero from './components/Hero'
-
-import Footer from './components/Footer'
 import About from './components/About'
 
 function App() {
