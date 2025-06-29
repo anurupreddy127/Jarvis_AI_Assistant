79d936cd49fa6e307f4045cb029004e0eb1c4d9b
Anurup Reddy <101058877+anurupreddy127@users.noreply.github.com>
2025-06-29 15:47:57 -0500

    Updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from './components/Header'
 import Hero from './components/Hero'
 import About from './components/About'
+import Features from './components/Features'
 
 function App() {
   return (
@@ -10,7 +11,7 @@ function App() {
       <main>
         <Hero />
         <About />
-
+        <Features />
       </main>
 
     </div>
