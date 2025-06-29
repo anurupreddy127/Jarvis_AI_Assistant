72063a1866904bf54f365c8b517aeb231266791d
Anurup Reddy <101058877+anurupreddy127@users.noreply.github.com>
2025-06-29 15:13:24 -0500

    Updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
 import React from 'react'
 import Header from './components/Header'
 import Hero from './components/Hero'
-import Features from './components/Features'
-import Demo from './components/Demo'
-import Download from './components/Download'
+
 import Footer from './components/Footer'
 import About from './components/About'
 
