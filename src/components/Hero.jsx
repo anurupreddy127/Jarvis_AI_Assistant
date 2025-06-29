4f09787da0b07a8a93e1e1252c3307a8289f15ca
Anurup Reddy <101058877+anurupreddy127@users.noreply.github.com>
2025-06-29 14:04:34 -0500

    Updated Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ export default function Hero() {
   return (
     <section className="hero">
       {/* full-screen 3D canvas */}
-      <div className="hero-canvas">
+      <div className="hero-background hero-canvas">
         <HumanFigure />
       </div>
 
