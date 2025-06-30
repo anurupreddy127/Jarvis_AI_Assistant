2f9140554a5eabcb5a5fa31764af161f7db785bb
Anurup Reddy <101058877+anurupreddy127@users.noreply.github.com>
2025-06-30 06:49:15 -0500

    Updated SubscribePage.jsx

---
+++ src/components/SubscribePage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function SubscribePage() {
+  return (
+    <section style={{ minHeight: '100vh', background: '#111', color: '#fff', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+      <h2>Loading subscription plans...</h2>
+      {/* RevenueCat subscription UI will go here */}
+    </section>
+  )
+}
